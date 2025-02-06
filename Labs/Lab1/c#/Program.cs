7aba8367806941465abc5142b975929b18fe90ef
illya_mak <illyamak24@gmail.com>
2025-02-06 20:24:42 +0200

    fixed problem with empty text on C#

--- Labs/Lab1/c#/Program.cs
+++ Labs/Lab1/c#/Program.cs
@@ -7,7 +7,7 @@ namespace LineApp
         static void Main(string[] args)
         {
             Console.WriteLine("Enter some text:");
-            string text = Console.ReadLine();
+            string text = Console.ReadLine() ?? string.Empty;
 
             Line line = new Line(text);
 
