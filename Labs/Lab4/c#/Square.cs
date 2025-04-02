c30650d3c717c55890c3a1e7eaf3ccb41e79d57a
illya_mak <illyamak24@gmail.com>
2025-04-03 00:36:04 +0300

    added C# part

--- Labs/Lab4/c#/Square.cs
+++ Labs/Lab4/c#/Square.cs
@@ -5,13 +5,11 @@ namespace FiguresApp
     {
         private int _sideLength;
 
-        // Constructor using another Square instance
         public Square(Square other) : base(other)
         {
             _sideLength = other.GetSideLength();
         }
 
-        // Constructor using a point and side length
         public Square((int, int) vertex, int sideLength = 0) : base(vertex, (vertex.Item1 + sideLength, vertex.Item2),
                                                                   (vertex.Item1 + sideLength, vertex.Item2 - sideLength),
                                                                   (vertex.Item1, vertex.Item2 - sideLength))
